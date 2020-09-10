8824fd3f32d1dc9cd1fa29d3f4bd846de101bd79
rakhimishra <mishrarakhi21@gmail.com>
2020-09-10 14:52:29 +0530

    Updates

---
+++ precache-manifest.20470cbb0968889220b6dc41de645496.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a284686ab3ced2419763f8e25f9666",
+    "url": "/healthsetgo/index.html"
+  },
+  {
+    "revision": "a9b7a3bf788896bbbf04",
+    "url": "/healthsetgo/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "74d4c4e477b1b8b26ec0",
+    "url": "/healthsetgo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9b7a3bf788896bbbf04",
+    "url": "/healthsetgo/static/js/2.644cb89d.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "/healthsetgo/static/js/2.644cb89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d4c4e477b1b8b26ec0",
+    "url": "/healthsetgo/static/js/main.f631090d.chunk.js"
+  },
+  {
+    "revision": "d80edf420c6d2943a660",
+    "url": "/healthsetgo/static/js/runtime-main.f99c260b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/healthsetgo/static/media/logo.5d5d9eef.svg"
+  }
+]);
