4b76b87c24ec5578e5d208e4e08268df8e2e6131
rakhimishra <mishrarakhi21@gmail.com>
2020-09-11 13:53:21 +0530

    pagination added

--- src/components/ExportCSV.js
+++ src/components/ExportCSV.js
@@ -1,24 +1,35 @@
 import React from 'react'
+import { CSVLink } from 'react-csv'
 import Button from 'react-bootstrap/Button';
-import * as FileSaver from 'file-saver';
-import * as XLSX from 'xlsx';
-
 const ExportCSV = ({csvData, fileName}) => {
-
-    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    const fileExtension = '.xlsx';
-
-    const exportToCSV = (csvData, fileName) => {
-        const ws = XLSX.utils.json_to_sheet(csvData);
-        const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
-        const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
-        const data = new Blob([excelBuffer], {type: fileType});
-        FileSaver.saveAs(data, fileName + fileExtension);
-    }
-
     return (
-        <Button variant="warning" onClick={(e) => exportToCSV(csvData,fileName)}>Export</Button>
+        <Button variant="warning">
+            <CSVLink data={csvData} filename={fileName}>Export</CSVLink>
+        </Button>
     )
 }
 
-export default ExportCSV
+// import React from 'react'
+// import Button from 'react-bootstrap/Button';
+// import * as FileSaver from 'file-saver';
+// import * as XLSX from 'xlsx';
+
+// const ExportCSV = ({csvData, fileName}) => {
+
+//     const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+//     const fileExtension = '.xlsx';
+
+//     const exportToCSV = (csvData, fileName) => {
+//         const ws = XLSX.utils.json_to_sheet(csvData);
+//         const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
+//         const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+//         const data = new Blob([excelBuffer], {type: fileType});
+//         FileSaver.saveAs(data, fileName + fileExtension);
+//     }
+
+//     return (
+//         <Button variant="warning" onClick={(e) => exportToCSV(csvData,fileName)}>Export</Button>
+//     )
+// }
+
+export default ExportCSV;
--- src/components/Tables.js
+++ src/components/Tables.js
@@ -2,23 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 import Data from "./Data";
 import { Button } from "react-bootstrap";
-import ExportCSV from "./ExportCSV";
+import { ExportCSV } from "./ExportCSV";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
 export default function Tables() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
   const [student, setStudent] = useState("");
   const [teachers, setTeachers] = useState("");
-  const [offset, setOffset] = useState(0);
-  const [perPage, setPerPage] = useState(10);
-  const [currentPage, setCurrentPage] = useState("");
-  const [totalPages, setTotalPages] = useState("");
-
-   
-
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
   useEffect(() => {
     fetch(
       "https://api.healthsetgo.info/api/v2/candidate/assignment/community/search",
@@ -30,14 +22,13 @@ export default function Tables() {
             "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhZGRyZXNzIjpudWxsLCJyb2xlIjpbeyJyb2xlX3R5cGVfaWQiOjEwLCJjcmVhdGVkX3RpbWUiOiIyMDE3LTEwLTA0VDA3OjQ0OjI0LjI4NDUxIiwibW9kaWZpZWRfdGltZSI6IjIwMTctMTAtMDRUMDc6NDQ6MjQuMjg0NTEiLCJ1c2VyX2lkIjoxLCJyb2xlX2Rlc2MiOiJHdWVzdCIsImlkIjoxLCJyb2xlX2FjdGl2ZSI6MX1dLCJwcm9maWxlIjpudWxsLCJjdXN0b20iOm51bGwsImV4dF9wcm92aWRlcl9pZCI6bnVsbCwidXVpZCI6IkhTR18xIiwidG9rZW4iOm51bGwsInVzZXJfYWN0aXZlIjoxLCJwaG9uZSI6bnVsbCwib3JnYW5pemF0aW9uIjpudWxsLCJpZCI6MSwiaWF0IjoxNTA3MTE1ODg1LCJlbWFpbCI6bnVsbCwidXNlcm5hbWUiOiJndWVzdHVzZXJAaHNnLmNvbSIsInN0YXR1cyI6MjAwfQ==.P1iOu3IgTvv5WvinQ7yEIZCQC2bg58QF8RJAq82T_aU=",
           "x-select": '{"inRoleTypeId": 20, "inLimit": 10, "inOffset":0}',
         },
-        // body: JSON.stringify(data),
+    
       }
     )
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", JSON.parse(data.results[0].users));
         console.log(data.results[0].count);
-        // setCount(data.results[0].count);
         setTeachers(data.results[0].count);
         setIsLoaded(true);
         setItems(JSON.parse(data.results[0].users));
@@ -58,9 +49,8 @@ export default function Tables() {
           "Content-Type": "application/json",
           authorization:
             "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhZGRyZXNzIjpudWxsLCJyb2xlIjpbeyJyb2xlX3R5cGVfaWQiOjEwLCJjcmVhdGVkX3RpbWUiOiIyMDE3LTEwLTA0VDA3OjQ0OjI0LjI4NDUxIiwibW9kaWZpZWRfdGltZSI6IjIwMTctMTAtMDRUMDc6NDQ6MjQuMjg0NTEiLCJ1c2VyX2lkIjoxLCJyb2xlX2Rlc2MiOiJHdWVzdCIsImlkIjoxLCJyb2xlX2FjdGl2ZSI6MX1dLCJwcm9maWxlIjpudWxsLCJjdXN0b20iOm51bGwsImV4dF9wcm92aWRlcl9pZCI6bnVsbCwidXVpZCI6IkhTR18xIiwidG9rZW4iOm51bGwsInVzZXJfYWN0aXZlIjoxLCJwaG9uZSI6bnVsbCwib3JnYW5pemF0aW9uIjpudWxsLCJpZCI6MSwiaWF0IjoxNTA3MTE1ODg1LCJlbWFpbCI6bnVsbCwidXNlcm5hbWUiOiJndWVzdHVzZXJAaHNnLmNvbSIsInN0YXR1cyI6MjAwfQ==.P1iOu3IgTvv5WvinQ7yEIZCQC2bg58QF8RJAq82T_aU=",
-          "x-select": '{"inRoleTypeId": 9, "inLimit": 10, "inOffset":0}',
+          "x-select": '{"inRoleTypeId": 9, "inLimit": 188, "inOffset":0}',
         },
-        // body: JSON.stringify(data),
       }
     )
       .then((response) => response.json())
@@ -89,7 +79,6 @@ export default function Tables() {
             "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhZGRyZXNzIjpudWxsLCJyb2xlIjpbeyJyb2xlX3R5cGVfaWQiOjEwLCJjcmVhdGVkX3RpbWUiOiIyMDE3LTEwLTA0VDA3OjQ0OjI0LjI4NDUxIiwibW9kaWZpZWRfdGltZSI6IjIwMTctMTAtMDRUMDc6NDQ6MjQuMjg0NTEiLCJ1c2VyX2lkIjoxLCJyb2xlX2Rlc2MiOiJHdWVzdCIsImlkIjoxLCJyb2xlX2FjdGl2ZSI6MX1dLCJwcm9maWxlIjpudWxsLCJjdXN0b20iOm51bGwsImV4dF9wcm92aWRlcl9pZCI6bnVsbCwidXVpZCI6IkhTR18xIiwidG9rZW4iOm51bGwsInVzZXJfYWN0aXZlIjoxLCJwaG9uZSI6bnVsbCwib3JnYW5pemF0aW9uIjpudWxsLCJpZCI6MSwiaWF0IjoxNTA3MTE1ODg1LCJlbWFpbCI6bnVsbCwidXNlcm5hbWUiOiJndWVzdHVzZXJAaHNnLmNvbSIsInN0YXR1cyI6MjAwfQ==.P1iOu3IgTvv5WvinQ7yEIZCQC2bg58QF8RJAq82T_aU=",
           "x-select": '{"inRoleTypeId": 9, "inLimit": 293, "inOffset":0}',
         },
-        // body: JSON.stringify(data),
       }
     )
       .then((response) => response.json())
@@ -99,13 +88,6 @@ export default function Tables() {
         setIsLoaded(true);
         setItems(JSON.parse(data.results[0].users));
 
-        const dataItems = JSON.parse(data.results[0].users)
-        console.log(dataItems)
-        const slice = dataItems.slice(offset,offset +perPage);
-        const pageCount = Math.ceil(dataItems.length / perPage);
-        setItems(JSON.parse(data.results[0].users));
-        
-        console.log(slice)
       })
       .catch((error) => {
         setIsLoaded(true);
@@ -125,13 +107,12 @@ export default function Tables() {
             "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhZGRyZXNzIjpudWxsLCJyb2xlIjpbeyJyb2xlX3R5cGVfaWQiOjEwLCJjcmVhdGVkX3RpbWUiOiIyMDE3LTEwLTA0VDA3OjQ0OjI0LjI4NDUxIiwibW9kaWZpZWRfdGltZSI6IjIwMTctMTAtMDRUMDc6NDQ6MjQuMjg0NTEiLCJ1c2VyX2lkIjoxLCJyb2xlX2Rlc2MiOiJHdWVzdCIsImlkIjoxLCJyb2xlX2FjdGl2ZSI6MX1dLCJwcm9maWxlIjpudWxsLCJjdXN0b20iOm51bGwsImV4dF9wcm92aWRlcl9pZCI6bnVsbCwidXVpZCI6IkhTR18xIiwidG9rZW4iOm51bGwsInVzZXJfYWN0aXZlIjoxLCJwaG9uZSI6bnVsbCwib3JnYW5pemF0aW9uIjpudWxsLCJpZCI6MSwiaWF0IjoxNTA3MTE1ODg1LCJlbWFpbCI6bnVsbCwidXNlcm5hbWUiOiJndWVzdHVzZXJAaHNnLmNvbSIsInN0YXR1cyI6MjAwfQ==.P1iOu3IgTvv5WvinQ7yEIZCQC2bg58QF8RJAq82T_aU=",
           "x-select": '{"inRoleTypeId": 20, "inLimit": 293, "inOffset":0}',
         },
-        // body: JSON.stringify(data),
       }
     )
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", JSON.parse(data.results[0].users));
-        
+
         setTeachers(data.results[0].count);
         setIsLoaded(true);
         setItems(JSON.parse(data.results[0].users));
@@ -142,6 +123,16 @@ export default function Tables() {
         console.error("Error:", error);
       });
   };
+
+  const columns = [
+    { dataField: "name", text: "Student name" },
+    { dataField: "class", text: "class" },
+    { dataField: "hsgId", text: "HSG ID" },
+    { dataField: "userClass", text: " User class" },
+    { dataField: "userId", text: " User ID" },
+    { dataField: "userRole", text: " User Role" },
+    { dataField: "riskStatus", text: "Risk Status" },
+  ];
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -163,36 +154,16 @@ export default function Tables() {
         </Button>
 
         <Data student={student} teachers={teachers} />
-        <ExportCSV />
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Class</th>
-              <th>HSGID</th>
-              <th>UserID</th>
-              <th>Risk Status</th>
-              <th>Role no </th>
-              <th>User Class</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => (
-              <tr key={index}>
-                <td>{item.name}</td>
-                <td>{item.class}</td>
-                <td>{item.hsgId}</td>
-                <td>{item.userId}</td>
-                <td>{item.riskStatus}</td>
-                <td>{item.userRole}</td>
-                <td>{item.userClass}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        <BootstrapTable
+          keyField="hsgId"
+          data={items}
+          columns={columns}
+          pagination={paginationFactory()}
+        />
+        <div className="col-md-4 center">
+          {/* <ExportCSV csvData={this.state.customers} fileName={this.state.fileName} /> */}
+        </div>
       </>
-
-      // {items}
     );
   }
 }
